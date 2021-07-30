1411f2d80ff0a7c30a3e8dde3da966a3dde189f9
Francisco Kahil <codexico@gmail.com>
2021-07-30 14:50:19 +0300

    moving towards second generation

---
+++ hello_world/params.py
@@ -0,0 +1,5 @@
+# we store some fixec params here
+GENERATIONAL_TABLE_NAME = 'generationalData'
+ZERO_GENERATION_TABLE_NAME = 'raw'
+GENERATIONAL_INDEX='category-persona-index'
+SINGLE_UNIT_INDEX='sku-index'
