9067ec0d177e464d09d868c71f81e9a8e2e917b6
Francisco Kahil <codexico@gmail.com>
2021-11-02 06:18:06 +0300

    upd_before_trans

--- code/params.py
+++ code/params.py
@@ -1,5 +1,7 @@
 # we store some fixec params here
 GENERATIONAL_TABLE_NAME = 'generationalData'
 ZERO_GENERATION_TABLE_NAME = 'raw'
-GENERATIONAL_INDEX='category-persona-index'
-SINGLE_UNIT_INDEX='sku-index'
+GENERATIONAL_INDEX = 'category-persona-index'
+CATEGORICAL_INDEX = 'CategoryIndex'
+SINGLE_UNIT_INDEX = 'sku-index'
+MIN_LENGTH = 12
--- code/random_generational_bundle.py
+++ code/random_generational_bundle.py
@@ -5,6 +5,7 @@ import params
 import random
 from pprint import pprint
 from app import uresp, DecimalEncoder
+
 dynamodb = boto3.resource("dynamodb")
 
 
@@ -17,18 +18,31 @@ def get_bundle_by_catper(category, persona_id, generation):
         ReturnConsumedCapacity="TOTAL",
         FilterExpression=Key('generation').eq(generation)
     )
-    print('Capacity consumed', response.get('ConsumedCapacity'))
+    print('Capacity consumed by get_bundle_by_catper', response.get('ConsumedCapacity'))
     items = response["Items"]
     return items
 
 
+def get_bundle_by_cat_exl_per(category, persona_id, generation):
+    table = dynamodb.Table(params.GENERATIONAL_TABLE_NAME)
+    response = table.query(
+        IndexName='category-generation-index',
+        KeyConditionExpression=Key('category').eq(category) & Key("generation").eq(generation),
+        ReturnConsumedCapacity="TOTAL",
+        FilterExpression= Attr("persona_id").ne(persona_id)
+    )
+    print('Capacity consumed by get_bundle_by_cat_exl_per', response.get('ConsumedCapacity'))
+    items = response["Items"]
+
+    return items
+
+
 def skus_by_gen(category, persona_id, generation, n):
-    print("JOPA", category, persona_id, generation, n)
     candidates = get_bundle_by_catper(category, persona_id, generation)
     how_many = min(len(candidates), n)
     nums = random.sample(candidates, how_many)
     res = [j for i in nums for j in i.get('shoppingCart')]
-    random.shuffle(res)
+    res = list(set(res))
     return res
 
 
@@ -49,6 +63,28 @@ def objs_by_skus(skus):
     return res
 
 
+def single_random_bundle(category, persona_id, generation, ):
+    all_others = get_bundle_by_cat_exl_per(category, persona_id, generation, )
+    num = random.choice(all_others)
+    res = [j for j in num.get('shoppingCart')]
+    return res
+
+
+def get_random_items_zero(category, n):
+    table = dynamodb.Table(params.ZERO_GENERATION_TABLE_NAME)
+    r = uuid4()
+    response = table.query(
+        KeyConditionExpression=Key("Caboodle Category").eq(category)
+                               & Key("item_id").gt(r.int >> 64),
+        Limit=n,
+        ReturnConsumedCapacity="TOTAL",
+    )
+    print('Capacity consumed', response.get('ConsumedCapacity'))
+    items = response["Items"]
+    items = [i.get('SKU') for i in items]
+    return items
+
+
 def random_generational_response(event, context):
     urlqs = event["queryStringParameters"]
     category = urlqs.get("category")
@@ -57,9 +93,25 @@ def random_generational_response(event, context):
     n = int(urlqs.get("n"))
 
     skus = skus_by_gen(category, persona_id, generation, n)
+    if len(skus) < params.MIN_LENGTH:
+        skus.extend(single_random_bundle(category, persona_id, generation, ))
+    skus = list(set(skus))
+    print(f'Plan to add: {params.MIN_LENGTH - len(skus)}')
+    while len(skus) < params.MIN_LENGTH:
+        rest = params.MIN_LENGTH - len(skus)
+        skus.extend(get_random_items_zero(category, rest))
+        skus = list(set(skus))
     objs = objs_by_skus(skus)
-    return uresp(objs)
+    unique_skus =[]
+    res = []
+    for i in objs:
+        if i.get('SKU') not in unique_skus:
+            unique_skus.append(i.get('SKU'))
+            res.append(i)
+
+    random.shuffle(res)
+    return uresp(res)
 
 
 if __name__ == '__main__':
-    print(skus_by_gen( 'Fruit',1 ,0 ,1))
+    print(skus_by_gen('Fruit', 1, 0, 1))
